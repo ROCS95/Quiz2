58688df6ca88e3723e8a664cb1315ade4bfd3552
Rocs95 <chacon9595@hotmail.com>
2016-08-10 20:52:40 -0500

    Listo

---
+++ QuizBackEnd/FrontEnd/Scripts/items.js
@@ -0,0 +1,23 @@
+﻿// objteto tipo Resource
+var Resource = function (resource) {
+    this.resource = resource;
+    this.getAll = function () {
+        return $.ajax({ url: 'http://localhost:51942/api/' + this.resource });
+    };
+    this.create = function (ItemDTO) {
+        return $.ajax({
+            type: "POST",
+            url: 'http://localhost:51942/api/' + this.resource,
+            data: ItemDTO
+        });
+    };
+    this.delete = function (id) {
+        return $.ajax({
+            type: "DELETE",
+            url: 'http://localhost:51942/api/' + this.resource + '/' + id
+        });
+    };
+};
+
+//creamos uno por cada entidad de objeto creado arriba que recibe el nombre de la entidad
+var Pokemon = new Resource('Item');
---
+++ QuizBackEnd/FrontEnd/Scripts/source.js
@@ -0,0 +1,23 @@
+﻿// objteto tipo Resource
+var Resource = function (resource) {
+    this.resource = resource;
+    this.getAll = function () {
+        return $.ajax({ url: 'http://localhost:51942/api/' + this.resource });
+    };
+    this.create = function (pokemonDTO) {
+        return $.ajax({
+            type: "POST",
+            url: 'http://localhost:51942/api/' + this.resource,
+            data: pokemonDTO
+        });
+    };
+    this.delete = function (id) {
+        return $.ajax({
+            type: "DELETE",
+            url: 'http://localhost:51942/api/' + this.resource + '/' + id
+        });
+    };
+};
+
+//creamos uno por cada entidad de objeto creado arriba que recibe el nombre de la entidad
+var Pokemon = new Resource('Pokemon');
